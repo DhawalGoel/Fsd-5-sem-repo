ca78dd191f1ff5a72bebad459e10a4c5cef00945
DhawalGoel <goeldhawal5@gmail.com>
2026-09-24 10:24:29 +0530

    .

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import Student from './components/Student.jsx'
  const App = () => {
   return (
-    <div>
-      
-      <Student />
+    <div style={{ display :'flex', gap:'10px' }}>
+
+      <Student name="Alice" class="10th Grade" roll={1} />
+      <br />
+      <Student name="Bob" class="11th Grade" roll={2} />
+      <br />
+      <Student name="Charlie" class="12th Grade" roll={3} />
+      <br />
+
     </div>
   )
 }
